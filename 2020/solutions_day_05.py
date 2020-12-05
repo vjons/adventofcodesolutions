2b9e88aeebd9c400bc4565add46f1effe4e22b33
vjons <vjonskth@gmail.com>
2020-12-05 06:40:48 +0100

    Solution day 5 finished

--- 2020/solutions_day_05.py
+++ 2020/solutions_day_05.py
@@ -1,26 +1,19 @@
 import aoc_lib as al
 import re
+import numpy as np
 
 raw=al.get_aoc_input(al.session_cookie,day=5).strip("\n")
+data=raw.replace("B","1").replace("F","0").replace("R","1").replace("L","0").split("\n")
 
-data=[dict([field.split(":") for field in part.replace("\n"," ").split(" ")])\
-      for part in raw.split("\n\n")]
+x,y=np.array([[int(d[:-3],2),int(d[-3:],2)] for d in data]).T
 
-# fields="byr","iyr","eyr","hgt","hcl","ecl","pid"
+IDS=np.array((x*8+y))
 
-# def validate1(pp):
-#     return all(k in pp for k in fields)
+remaining_IDS=np.array(list(set(list(range(7,127*8)))-set(list(IDS))))
+filt=np.gradient(remaining_IDS)>1
+your_ID=remaining_IDS[filt][1]
 
-# def validate2(pp):
-#     patterns="19[2-9][0-9]|200[0-2]","20(1[0-9]|20)","20(2[0-9]|30)",\
-#              "(1[5-8][0-9]|19[0-3])cm|(59|6[0-9]|7[0-6])in",\
-#              "#([0-9]|[a-f]){6}","amb|blu|brn|gry|grn|hzl|oth","[0-9]{9}"
-#     return all(re.match(pattern,pp[key]) for pattern,key in zip(patterns,fields))
+answer1=np.max(IDS)
+answer2=your_ID
 
-# valid_pps_1=list(filter(validate1,data))
-# valid_pps_2=list(filter(validate2,valid_pps_1))
-
-# answer1=len(valid_pps_1)
-# answer2=len(valid_pps_2)
-
-# print("answer 1:",answer1,"answer 2:",answer2)
+print("answer 1:",answer1,"answer 2:",answer2)
