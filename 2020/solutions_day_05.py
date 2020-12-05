a35e7d6bd4cc4053815ccf3157ffde77881bbd0d
vjons <vjonskth@gmail.com>
2020-12-05 08:40:37 +0100

    Update solutions_day_05.py

--- 2020/solutions_day_05.py
+++ 2020/solutions_day_05.py
@@ -1,15 +1,16 @@
 import aoc_lib as al
 import numpy as np
+import re
 
 raw=al.get_aoc_input(al.session_cookie,day=5).strip("\n")
-data=raw.replace("B","1").replace("F","0").replace("R","1").replace("L","0").split("\n")
+data=re.sub("F|L","0",re.sub("B|R","1",raw)).split("\n")
 
-IDS=np.array([int(d[:-3],2)*8+int(d[-3:],2) for d in data]).T
+IDS=np.array([int(d,2) for d in data]).T
 
 missing_IDS=np.array(list(set(range(7,127*8))-set(IDS)))
 
-filt=np.gradient(missing_IDS)>1
-your_ID=missing_IDS[filt][1]
+filt=np.diff(missing_IDS)>1
+your_ID=missing_IDS[:-1][filt][1]
 
 answer1=np.max(IDS)
 answer2=your_ID
