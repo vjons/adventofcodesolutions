180c3f2abfd7079ef11ea8a4edf4b4f5f77a2005
vjons <vjonskth@gmail.com>
2020-12-12 22:03:26 +0100

    Update solutions_day_10.py

--- 2020/solutions_day_10.py
+++ 2020/solutions_day_10.py
@@ -8,7 +8,7 @@ def nways(n,k):
     return 2**(n-1-k)*(2**k-n+k)
 
 def answers(raw):
-    data=np.sort(np.array(raw.split("\n")))
+    data=np.sort(np.array(raw.split("\n"),dtype=np.uint64))
     diff=np.r_[1,np.diff(data),3].astype(np.uint64)
     a1=np.sum(diff==1)*np.sum(diff==3)
     a2=np.prod([nways(len(s),3) for s in "".join(diff.astype(str)).split("3")],dtype=np.uint64)
