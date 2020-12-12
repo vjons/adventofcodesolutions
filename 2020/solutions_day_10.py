0d300f16f6a0b00633ece416f821dc88006954e9
vjons <vjonskth@gmail.com>
2020-12-12 22:02:17 +0100

    analythic expression for part 2

--- 2020/solutions_day_10.py
+++ 2020/solutions_day_10.py
@@ -5,12 +5,10 @@ import itertools as itr
 def nways(n,k):
     if n<2:
         return 1
-    return 2**(n-1)-sum(n-k-m for m in range(n-k))
+    return 2**(n-1-k)*(2**k-n+k)
 
-# nways=[1,1,2,4,7]
 def answers(raw):
-    global data,diff
-    data=np.sort(np.array(raw.split("\n"),dtype=np.uint64))
+    data=np.sort(np.array(raw.split("\n")))
     diff=np.r_[1,np.diff(data),3].astype(np.uint64)
     a1=np.sum(diff==1)*np.sum(diff==3)
     a2=np.prod([nways(len(s),3) for s in "".join(diff.astype(str)).split("3")],dtype=np.uint64)
