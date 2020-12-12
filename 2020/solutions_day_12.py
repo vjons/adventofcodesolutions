dd7b70041e0d21a1060d32e455c6b82e8831aceb
vjons <vjonskth@gmail.com>
2020-12-12 22:51:50 +0100

    day12 done

---
+++ 2020/solutions_day_12.py
@@ -0,0 +1,25 @@
+import aoc_lib as al
+import numpy as np
+import itertools as itr
+from scipy.ndimage import convolve
+import time
+
+cmds=dict(N=(1j,1),S=(-1j,1),E=(1,1),W=(-1,1),L=(0,1j),R=(0,-1j))
+
+def answers(raw):
+    instructions=[(r[0],int(r[1:])) for r in raw.split("\n")]
+    for part2,d in enumerate((1,10+1j)): 
+        z=0j
+        for inst,val in instructions:
+            if inst=="F":
+                z+=d*val
+                continue
+            c,m=cmds[inst]
+            d*=m**(val/90)
+            if part2:
+                d+=c*val
+            else:
+                z+=c*val
+        yield int(abs(z.real)+abs(z.imag))
+
+al.present_answers(12,answers)
