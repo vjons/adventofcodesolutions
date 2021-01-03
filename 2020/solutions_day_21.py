89c5479d37263f02468fc0b78359abf7c2a64267
vjons <vjonskth@gmail.com>
2021-01-04 00:50:45 +0100

    update day 21

--- 2020/solutions_day_21.py
+++ 2020/solutions_day_21.py
@@ -1,15 +1,14 @@
 import aoc_lib as al
 
 def parse(r):
-    f,a=r.split(" (contains ")
-    a=a.replace(" ","").strip(")").split(",")
+    f,a=r[:-1].split(" (contains")
+    a=a.replace(" ","").split(",")
     return set(f.split()),set(a)
 
 def answers(raw):
-    data=[parse(r) for r in raw.split("\n")]
-    foods,alles=zip(*data)
-    all_ings,all_alls=(set.union(*x) for x in (foods,alles))
-    uniques={key:set.intersection(*(f for f,a in data if key in a))\
+    data=list(zip(*(parse(r) for r in raw.split("\n"))))
+    all_ings,all_alls=(set.union(*x) for x in data)
+    uniques={key:set.intersection(*(f for f,a in zip(*data) if key in a))\
              for key in all_alls}
 
     free_ai=[]
@@ -21,7 +20,7 @@ def answers(raw):
     _,free_ings=zip(*sorted(free_ai))
     dang_ings=all_ings-set(free_ings)
 
-    yield sum(sum(1 for ing in food if ing in dang_ings) for food in foods)
+    yield sum(sum(1 for ing in food if ing in dang_ings) for food in data[0])
     yield ",".join(free_ings)
 
 if __name__=="__main__":
