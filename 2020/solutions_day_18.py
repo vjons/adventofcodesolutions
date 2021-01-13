e0f47c862d185fd41c04dc1a94ff8473c88f90fc
vjons <vjonskth@gmail.com>
2021-01-13 22:28:13 +0100

    solution day 18 (to perfection)

--- 2020/solutions_day_18.py
+++ 2020/solutions_day_18.py
@@ -1,51 +1,24 @@
 import aoc_lib as al
-import numpy as np
-import itertools as itr
-from operator import add,mul
+import operator as op
 
-ops={"*":mul,"+":add,"R":mul,"L":mul}
-ids={"*":1,"+":0}
+operators = {"*":op.mul,"+":op.add}
+precedences = ("LR","+*","*+","RL")
 
-#Did it first then tried to clean up the code and broke it
-#Will fix it later
 def evaluate(data,prec):
-    pass
-    # ,op="+",term=0,factor=None):
-#     if factor is None:
-#         factor = 0 if prec=="*+" else 1
-#     try:
-#         char=next(data)
-#     except:
-#         return factor+term if prec=="*+" else factor*term
-#     if char in "+*":
-#         op=char
-#     elif char not in (" )"):
-#         if char=="(":
-#             pc=[1]
-#             f=lambda x: (pc.append(pcs.get(x,0)),sum(pc))[1]
-#             value=evaluate((d for d in itr.takewhile(f,data)),prec)
-#         else:
-#             value=int(char)
-#         if prec=="L2R":
-#             term=eval(f"term{op}value")
-#         elif prec=="+*":
-#             if op=="+":
-#                 term+=value
-#             else:
-#                 factor*=term
-#                 term=value
-#         elif prec=="*+":
-#             if op=="*":
-#                 factor*=value
-#             else:
-#                 term+=factor
-#                 factor=value
-
-#     return evaluate(data,prec,op,term,factor)
+    operate = None
+    while (char:=next(data,")"))!=")":
+        if char in operators:
+            operate = operators[char]
+            if prec[-1] in (char,"L"):
+                return operate(acc,evaluate(data,prec))
+            continue
+        value = evaluate(data,prec) if char == "(" else int(char)
+        acc = value if operate is None else operate(acc,value)
+    return acc
 
 def answers(raw):
     data=[r.replace(" ","") for r in raw.split("\n")]
-    yield from (sum(evaluate(iter(d),prec) for d in data) for prec in ("LR","+*"))
+    yield from (sum(evaluate(iter(d),p) for d in data) for p in precedences)
 
 if __name__=="__main__":
     al.present_answers(2020,18,answers)
