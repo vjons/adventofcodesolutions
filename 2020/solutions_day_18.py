f3a301a8aef2f750858476b790064ee6adb48b25
vjons <vjonskth@gmail.com>
2021-01-31 22:14:26 +0100

    Update solutions_day_18.py

--- 2020/solutions_day_18.py
+++ 2020/solutions_day_18.py
@@ -1,19 +1,20 @@
 import aoc_lib as al
 import operator as op
 
-id_=lambda x,y:y
+id_=lambda x,y:x or y
 
-def evaluate(it,ops,pr0=0,acc=None,op_pr=(id_,0)):
-    while ((not acc) or ch not in ops or pr0<op_pr[1]) and (ch:=next(it,")"))!=")":
-        if (op_pr:=ops.get(ch,(op_pr[0],pr0)))[1]<=pr0:
-            acc = op_pr[0](acc,evaluate(it,ops,op_pr[1]) if ch in "(+*"  else int(ch))
+def evaluate(it,ops,acc=None,op=id_):
+    while (not acc or ch not in ops or pr>=-1 and pr!=1) and (ch:=next(it,"")):
+        op,pr=ops.get(ch.strip("("),(op,0))
+        if pr<=0: acc=op(acc,evaluate(it,ops) if ch in ops else int(ch))
     return acc
+
 
 def answers(raw):
     data=[r.replace(" ","") for r in raw.split("\n")]
-    opss= [{c:(f,p) for c,f,p in zip("+*)",(op.add,op.mul,id_,id_),pr+(10,))}\
+    opers= [{c:(f,p) for c,f,p in zip("+*)(",(op.add,op.mul,None,None),pr+(1,-1))}\
                for pr in [(2,2),(2,-2),(-2,2),(-2,-2)]]
-    yield from (sum(evaluate(iter(d),ops) for d in data) for ops in opss)
+    yield from (sum(evaluate(iter(d),ops) for d in data) for ops in opers)
 
 if __name__=="__main__":
     al.present_answers(2020,18,answers)
