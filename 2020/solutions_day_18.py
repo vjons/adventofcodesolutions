8a832a3e78e5807ddbc6250ae5b83c0f07200a64
vjons <vjonskth@gmail.com>
2021-01-31 15:52:54 +0100

    Update solutions_day_18.py

--- 2020/solutions_day_18.py
+++ 2020/solutions_day_18.py
@@ -1,24 +1,19 @@
 import aoc_lib as al
 import operator as op
 
-operators = {"*":op.mul,"+":op.add}
-precedences = ("LR","+*","*+","RL")
+id_=lambda x,y:y
 
-def evaluate(data,prec):
-    operate = None
-    while (char:=next(data,")"))!=")":
-        if char in operators:
-            operate = operators[char]
-            if prec[-1] in (char,"L"):
-                return operate(acc,evaluate(data,prec))
-            continue
-        value = evaluate(data,prec) if char == "(" else int(char)
-        acc = value if operate is None else operate(acc,value)
+def evaluate(it,ops,pr0=0,acc=None,op_pr=(id_,0)):
+    while ((not acc) or ch not in ops or pr0<op_pr[1]) and (ch:=next(it,")"))!=")":
+        if (op_pr:=ops.get(ch,(op_pr[0],pr0)))[1]<=pr0:
+            acc = op_pr[0](acc,evaluate(it,ops,op_pr[1]) if ch in "(+*"  else int(ch))
     return acc
 
 def answers(raw):
     data=[r.replace(" ","") for r in raw.split("\n")]
-    yield from (sum(evaluate(iter(d),p) for d in data) for p in precedences)
+    opss= [{c:(f,p) for c,f,p in zip("+*)",(op.add,op.mul,id_,id_),pr+(10,))}\
+               for pr in [(2,2),(2,-2),(-2,2),(-2,-2)]]
+    yield from (sum(evaluate(iter(d),ops) for d in data) for ops in opss)
 
 if __name__=="__main__":
     al.present_answers(2020,18,answers)
