342e71684b5362f9213de4b63e4f3d219ac32f09
vjons <vjonskth@gmail.com>
2021-02-08 23:02:57 +0100

    Update solutions_day_17.py

--- 2020/solutions_day_17.py
+++ 2020/solutions_day_17.py
@@ -4,13 +4,15 @@ from scipy.ndimage import convolve
 
 def answers(raw):
     init=np.array([list(r) for r in raw.split("\n")])=="#"
-    N=6
-    for D in (3,4):
-        active=np.pad(init[(np.newaxis,)*(D-init.ndim)],N)
-        for _ in range(N):
-            neighbors=convolve(active,np.ones((3,)*D),int,mode="constant")
-            active[:]=active&(neighbors==4) | (neighbors==3)
-        yield np.sum(active)
+    niter=6
+    dims=(2,3,4,5)
+    for D in dims:
+        state=np.pad(init[(np.newaxis,)*(D-init.ndim)],niter)
+        kernel=np.ones((3,)*D)
+        for _ in range(niter):
+            neighbors=convolve(state,kernel,int,mode="constant")
+            state[:]=state&(neighbors==4) | (neighbors==3)
+        yield state.sum()
 
 if __name__=="__main__":
     al.present_answers(2020,17,answers)
